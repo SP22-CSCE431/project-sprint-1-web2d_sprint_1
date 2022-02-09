19016f6501e02f434074ecdb38dce77637e125c4
JClark-TAMU <jonathan.clark@tamu.edu>
2022-02-09 08:32:24 -0600

    Changed 'int' to 'integer'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_020542) do
+ActiveRecord::Schema.define(version: 2022_02_09_140644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,18 @@ ActiveRecord::Schema.define(version: 2022_02_09_020542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "email"
+    t.boolean "isAdmin"
+    t.string "role"
+    t.string "bio"
+    t.integer "portfolioID"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
