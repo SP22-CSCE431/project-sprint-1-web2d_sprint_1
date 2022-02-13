7847477b70f333a0361fb65c51cfc4137ba6c2c0
JClark-TAMU <jonathan.clark@tamu.edu>
2022-02-13 13:40:02 -0600

    fix parameter bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user,:email, :role, :bio, :isAdmin).permit(:username, :email, :role, :bio, :isAdmin)
+      params.require(:user).permit(:username, :email, :role, :bio, :isAdmin)
     end
 end
